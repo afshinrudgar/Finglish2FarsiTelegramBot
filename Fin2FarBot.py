01715a8016b82f017f71fbb4e1dc0435cea6ee28
afshinrodgar <afshin.chichi@gmail.com>
2015-08-12 09:42:23 +0430

    some change happend

--- Fin2FarBot.py
+++ Fin2FarBot.py
@@ -7,16 +7,20 @@ import time
 import urllib3
 import certifi
 import logging
+import re
 
 http = urllib3.PoolManager(
     cert_reqs='CERT_REQUIRED',  # Force certificate check.
     ca_certs=certifi.where(),  # Path to the Certifi bundle.
 )
+urllib3.disable_warnings()
 
 en_dict = enchant.Dict('en')
 alphabet = set([i for i in 'abcdefghijklmnopqrstuvwxyz'])
 
-logging.basicConfig(filename='Fin2FarBotLog.log', level=logging.INFO)
+logging.basicConfig(filename='Fin2FarBotLog.log', filemode='a', level=logging.INFO)
+
+base_chat_id = None
 
 
 def transliterate(text):
@@ -37,8 +41,10 @@ def transliterate(text):
 
 
 def is_finglish(text):
-    flag = True
+    text = re.sub('\W+', ' ', text)
     s_t = text.split(' ')
+    while '' in s_t:
+        s_t.remove('')
     for i in range(min(3, len(s_t))):
         if not en_dict.check(s_t[i]) and alphabet.intersection(s_t[i]):
             return True
@@ -48,19 +54,21 @@ def listener(messages):
     """
         When new messages arrive TeleBot will call this function.
     """
+    global base_chat_id
     for m in messages:
         chatid = m.chat.id
-        if m.content_type == 'text' and is_finglish(m.text.lower()):
+        if m.content_type == 'text':
             print("[%s] %s %s (%s), %s: %s" %(time.ctime(), m.from_user.first_name, m.from_user.last_name, m.from_user.username, chatid, m.text))
             logging.info("[%s] %s %s (%s), %s: %s\n" %(time.ctime(), m.from_user.first_name, m.from_user.last_name, m.from_user.username, chatid, m.text))
-            bot.reply_to(
-                m,
-                transliterate(m.text)
-            )
+            if is_finglish(m.text.lower()):
+               bot.reply_to(
+                    m,
+                    transliterate(m.text.lower())
+                )
 
 
 log = open('Fin2FarBotLog.log', 'w')
-bot = telebot.TeleBot('122159953:AAGAxAZhIRki8J0vEKd8II9zQCqXV0UV67M')
+bot = telebot.TeleBot('TOKEN')
 bot.set_update_listener(listener)
 bot.polling()
 #Use none_stop flag let polling will not stop when get new message occur error.
