4b784245493c1bd06811acbd5d1792c142e80db9
afshinrodgar <afshin.chichi@gmail.com>
2015-08-13 12:40:50 +0430

    polling handler, data.p added

--- Fin2FarBot.py
+++ Fin2FarBot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python2
-from __future__ import print_function
+# coding=utf-8
+from __future__ import print_function, unicode_literals
 import telebot
 import requests
 import enchant
@@ -7,7 +8,24 @@ import time
 import urllib3
 import certifi
 import logging
+import pickle
 import re
+import sys
+
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+DATA = './data.p'
+
+global data
+try:
+    data = pickle.load(open(DATA, 'rb'))
+except IOError:
+    data = {}
+
+
+transliteration_set = set()
 
 http = urllib3.PoolManager(
     cert_reqs='CERT_REQUIRED',  # Force certificate check.
@@ -18,26 +36,49 @@ urllib3.disable_warnings()
 en_dict = enchant.Dict('en')
 alphabet = set([i for i in 'abcdefghijklmnopqrstuvwxyz'])
 
-logging.basicConfig(filename='Fin2FarBotLog.log', filemode='a', level=logging.INFO)
+logging.basicConfig(
+    filename='Fin2FarBotLog.log', filemode='a', level=logging.INFO)
 
 base_chat_id = None
 
 
 def transliterate(text):
-    url = 'http://www.behnevis.com/php/convert.php'
-    headers = {'Accept-Charset': 'UTF-8'}
+    words = set(re.findall('\w+', text))
 
-    r = http.request(
-        'POST',
-        url,  # + 'farsi=salam&responsetime=-1&resulttype=json',
-        fields={
-            'farsi': text,
-            'responsetime': -1
-        },
-        headers=headers
-    )
+    new_finglish = list(words.difference(data.keys()))
 
-    return r.data
+    print('new_finglish', new_finglish)
+
+    if new_finglish:
+        print("requesting for farsi")
+
+        url = 'http://www.behnevis.com/php/convert.php'
+        headers = {'Accept-Charset': 'UTF-8'}
+
+        r = http.request(
+            'POST',
+            url,  # + 'farsi=salam&responsetime=-1&resulttype=json',
+            fields={
+                'farsi': ' '.join(new_finglish),
+                # 'responsetime': -1
+            },
+            headers=headers
+        )
+
+        new_farsi = r.data.strip().split(' ')
+        print(new_finglish)
+        print(new_farsi)
+
+        for i, j in zip(new_finglish, new_farsi):
+            data[i] = j
+
+    print(data)
+
+    for word in words:
+        text = re.sub(word, data[word], text)
+
+    print('text', text)
+    return text
 
 
 def is_finglish(text):
@@ -50,31 +91,35 @@ def is_finglish(text):
             return True
     return False
 
+
 def listener(messages):
     """
         When new messages arrive TeleBot will call this function.
     """
-    global base_chat_id
     for m in messages:
         chatid = m.chat.id
         if m.content_type == 'text':
-            print("[%s] %s %s (%s), %s: %s" %(time.ctime(), m.from_user.first_name, m.from_user.last_name, m.from_user.username, chatid, m.text))
-            logging.info("[%s] %s %s (%s), %s: %s\n" %(time.ctime(), m.from_user.first_name, m.from_user.last_name, m.from_user.username, chatid, m.text))
+            print("[%s] %s %s (%s), %s: %s" % (time.ctime(), m.from_user.first_name,
+                                               m.from_user.last_name, m.from_user.username, chatid, m.text))
+            logging.info("[%s] %s %s (%s), %s: %s\n" % (time.ctime(
+            ), m.from_user.first_name, m.from_user.last_name, m.from_user.username, chatid, m.text))
             if is_finglish(m.text.lower()):
-               bot.reply_to(
+                bot.reply_to(
                     m,
                     transliterate(m.text.lower())
                 )
 
 
-log = open('Fin2FarBotLog.log', 'w')
-bot = telebot.TeleBot('TOKEN')
+bot = telebot.TeleBot('Token')
 bot.set_update_listener(listener)
-bot.polling()
-#Use none_stop flag let polling will not stop when get new message occur error.
+# Use none_stop flag let polling will not stop when get new message occur
+# error.
 bot.polling(none_stop=True)
-# Interval setup. Sleep 3 secs between request new message.
-bot.polling(interval=0)
 
-while True: # Don't let the main Thread end.
-    pass
+while True:  # Don't let the main Thread end.
+    try:
+        time.sleep(10)
+        bot.get_me()
+        pickle.dump(data, open(DATA, 'wb'))
+    except:
+        bot.polling(none_stop=True)
